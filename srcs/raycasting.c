5db86623dc641616aca593bb7d94b7949e9d5f0b
Sumon Md Ohiduzzaman <sumon@Sumons-Laptop.fritz.box>
2024-05-12 17:59:01 +0200

    v1.0

--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -6,119 +6,119 @@
 /*   By: msumon < msumon@student.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:29:21 by msumon            #+#    #+#             */
-/*   Updated: 2024/05/11 21:45:38 by msumon           ###   ########.fr       */
+/*   Updated: 2024/05/12 17:54:27 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	add_img_pixel(t_data *data, t_point p, int x, double y)
+void	add_image_pixel(t_data *data, t_point point, int x_coord, double y_coord)
 {
-    double	tx;
-    double	ty;
-    int		i;
+    double	texture_x;
+    double	texture_y;
+    int		index;
 
-    tx = (int)(p.x) % IMAGE_SIZE;
-    ty = (int)(p.y) % IMAGE_SIZE;
-    i = WIN_H / 2 - x - 1;
-    while (++i < WIN_H / 2 + x && i < WIN_H)
+    texture_x = (int)(point.x) % IMAGE_SIZE;
+    texture_y = (int)(point.y) % IMAGE_SIZE;
+    index = WIN_H / 2 - x_coord - 1;
+    while (++index < WIN_H / 2 + x_coord && index < WIN_H)
     {
-        if ((int)tx == 0)
-            data->img->pixels[WIN_W * i + data->ray_num]
-                = data->ea_img->pixels[(int)y * IMAGE_SIZE + (int)ty];
-        else if ((int)ty == 0)
-            data->img->pixels[WIN_W * i
-                + data->ray_num] = data->so_img->pixels[(int)y * IMAGE_SIZE
-                + IMAGE_SIZE - 1 - (int)tx];
-        else if ((int)tx == IMAGE_SIZE - 1)
-            data->img->pixels[WIN_W * i
-                + data->ray_num] = data->we_img->pixels[(int)y * IMAGE_SIZE
-                + IMAGE_SIZE - 1 - (int)ty];
-        else if ((int)ty == IMAGE_SIZE - 1)
-            data->img->pixels[WIN_W * i + data->ray_num]
-                = data->no_img->pixels[(int)y * IMAGE_SIZE + (int)tx];
-        y += data->coordinate_y;
+        if ((int)texture_x == 0)
+            data->img->pixels[WIN_W * index + data->ray_num]
+                = data->ea_img->pixels[(int)y_coord * IMAGE_SIZE + (int)texture_y];
+        else if ((int)texture_y == 0)
+            data->img->pixels[WIN_W * index
+                + data->ray_num] = data->so_img->pixels[(int)y_coord * IMAGE_SIZE
+                + IMAGE_SIZE - 1 - (int)texture_x];
+        else if ((int)texture_x == IMAGE_SIZE - 1)
+            data->img->pixels[WIN_W * index
+                + data->ray_num] = data->we_img->pixels[(int)y_coord * IMAGE_SIZE
+                + IMAGE_SIZE - 1 - (int)texture_y];
+        else if ((int)texture_y == IMAGE_SIZE - 1)
+            data->img->pixels[WIN_W * index + data->ray_num]
+                = data->no_img->pixels[(int)y_coord * IMAGE_SIZE + (int)texture_x];
+        y_coord += data->coordinate_y;
     }
 }
 
-void	draw_texture(t_data *data, double dist, t_point p)
+void	draw_texture(t_data *data, double distance, t_point point)
 {
-	int		x;
-	double	ty_off;
-	double	y;
+    int		x_coord;
+    double	texture_y_offset;
+    double	y_coord;
 
-	x = (int)(IMAGE_SIZE * WIN_H / 2 / dist);
-	ty_off = 0;
-	data->coordinate_y = 1024.0 / (double)(2 * x);
-	if (x > WIN_H / 2)
-	{
-		ty_off = (x - WIN_H / 2);
-		x = WIN_H / 2;
-	}
-	y = ty_off * data->coordinate_y;
-	add_img_pixel(data, p, x, y);
+    x_coord = (int)(IMAGE_SIZE * WIN_H / 2 / distance);
+    texture_y_offset = 0;
+    data->coordinate_y = 1024.0 / (double)(2 * x_coord);
+    if (x_coord > WIN_H / 2)
+    {
+        texture_y_offset = (x_coord - WIN_H / 2);
+        x_coord = WIN_H / 2;
+    }
+    y_coord = texture_y_offset * data->coordinate_y;
+    add_image_pixel(data, point, x_coord, y_coord);
 }
 
-void	draw_if_empty(t_data *data, t_point p1, double dx)
+void	draw_if_empty(t_data *data, t_point point1, double dx)
 {
-	if (data->map[(int)p1.y / IMAGE_SIZE]
-		[(int)p1.x / IMAGE_SIZE] == EMPTY)
-		p1.x -= dx;
-	draw_texture(data, sqrt(pow(data->player.y - p1.y, 2) + pow(data->player.x
-				- p1.x, 2)) * cos(abs(data->ray_angle)), p1);
+    if (data->map[(int)point1.y / IMAGE_SIZE]
+        [(int)point1.x / IMAGE_SIZE] == EMPTY)
+        point1.x -= dx;
+    draw_texture(data, sqrt(pow(data->player.y - point1.y, 2) + pow(data->player.x
+                - point1.x, 2)) * cos(abs(data->ray_angle)), point1);
 }
 
-void	draw_line(t_data *data, t_point p1, t_point p2, double length)
+void	draw_line(t_data *data, t_point point1, t_point point2, double length)
 {
-	double	dx;
-	double	dy;
-	double	max;
+    double	delta_x;
+    double	delta_y;
+    double	max_value;
 
-	dx = p2.x - p1.x;
-	dy = p2.y - p1.y;
-	max = fmax(fabs(dx), fabs(dy));
-	dy /= max;
-	dx /= max;
-	p2.x += dx * length;
-	p2.y += dy * length;
-	while (1)
-	{
-		if (IMAGE_SIZE * data->map_width > p1.x && IMAGE_SIZE
-			* data->map_height > p1.y && data->map[(int)p1.y
-				/ IMAGE_SIZE][(int)p1.x / IMAGE_SIZE] != EMPTY)
-		{
-			draw_if_empty(data, p1, dx);
-			break ;
-		}
-		p1.x += dx;
-		p1.y += dy;
-	}
+    delta_x = point2.x - point1.x;
+    delta_y = point2.y - point1.y;
+    max_value = fmax(fabs(delta_x), fabs(delta_y));
+    delta_y /= max_value;
+    delta_x /= max_value;
+    point2.x += delta_x * length;
+    point2.y += delta_y * length;
+    while (1)
+    {
+        if (IMAGE_SIZE * data->map_width > point1.x && IMAGE_SIZE
+            * data->map_height > point1.y && data->map[(int)point1.y
+                / IMAGE_SIZE][(int)point1.x / IMAGE_SIZE] != EMPTY)
+        {
+            draw_if_empty(data, point1, delta_x);
+            break ;
+        }
+        point1.x += delta_x;
+        point1.y += delta_y;
+    }
 }
 
-void	raycast(t_data *data, t_point dir, double length)
+void	raycast(t_data *data, t_point direction, double length)
 {
-	t_point	p;
-	double	del_x;
-	double	del_y;
+    t_point	point;
+    double	delta_x;
+    double	delta_y;
 
-	data->ray_num = -1;
-	p.x = data->player.x;
-	p.y = data->player.y;
-	p.color = 0x5500FF00;
-	del_x = (dir.x - data->player.x);
-	del_y = (dir.y - data->player.y);
-	dir.x = (del_x * cos(-FOV / 2) - del_y * sin(-FOV / 2)) + data->player.x;
-	dir.y = (del_x * sin(-FOV / 2) + del_y * cos(-FOV / 2)) + data->player.y;
-	data->ray_angle = -FOV / 2;
-	while (++data->ray_num < WIN_W)
-	{
-		draw_line(data, p, dir, length);
-		del_x = (dir.x - data->player.x);
-		del_y = (dir.y - data->player.y);
-		dir.x = (del_x * cos(FOV / WIN_W) - del_y * sin(FOV / WIN_W))
-			+ data->player.x;
-		dir.y = (del_x * sin(FOV / WIN_W) + del_y * cos(FOV / WIN_W))
-			+ data->player.y;
-		data->ray_angle += FOV / WIN_W;
-	}
+    data->ray_num = -1;
+    point.x = data->player.x;
+    point.y = data->player.y;
+    point.color = 0x5500FF00;
+    delta_x = (direction.x - data->player.x);
+    delta_y = (direction.y - data->player.y);
+    direction.x = (delta_x * cos(-FOV / 2) - delta_y * sin(-FOV / 2)) + data->player.x;
+    direction.y = (delta_x * sin(-FOV / 2) + delta_y * cos(-FOV / 2)) + data->player.y;
+    data->ray_angle = -FOV / 2;
+    while (++data->ray_num < WIN_W)
+    {
+        draw_line(data, point, direction, length);
+        delta_x = (direction.x - data->player.x);
+        delta_y = (direction.y - data->player.y);
+        direction.x = (delta_x * cos(FOV / WIN_W) - delta_y * sin(FOV / WIN_W))
+            + data->player.x;
+        direction.y = (delta_x * sin(FOV / WIN_W) + delta_y * cos(FOV / WIN_W))
+            + data->player.y;
+        data->ray_angle += FOV / WIN_W;
+    }
 }
--- srcs/rotations.c
+++ srcs/rotations.c
@@ -6,46 +6,46 @@
 /*   By: msumon < msumon@student.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:19:03 by msumon            #+#    #+#             */
-/*   Updated: 2024/05/10 21:46:42 by msumon           ###   ########.fr       */
+/*   Updated: 2024/05/12 17:57:34 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	rotate_vec(double *beg_x, double *beg_y, double *end_x, double *end_y)
+void	rotate_vector(double *start_x, double *start_y, double *end_x, double *end_y)
 {
-	const double	del_x = (*beg_x - *end_x);
-	const double	del_y = (*beg_y - *end_y);
+    const double	delta_x = (*start_x - *end_x);
+    const double	delta_y = (*start_y - *end_y);
 
-	*beg_x = (del_x * cos(ROTATE_SPEED) - del_y * sin(ROTATE_SPEED)) + *end_x;
-	*beg_y = (del_x * sin(ROTATE_SPEED) + del_y * cos(ROTATE_SPEED)) + *end_y;
+    *start_x = (delta_x * cos(ROTATE_SPEED) - delta_y * sin(ROTATE_SPEED)) + *end_x;
+    *start_y = (delta_x * sin(ROTATE_SPEED) + delta_y * cos(ROTATE_SPEED)) + *end_y;
 }
 
-void	rotate_vec_back(double *beg_x, double *beg_y, double *end_x,
-		double *end_y)
+void	rotate_vector_back(double *start_x, double *start_y, double *end_x,
+        double *end_y)
 {
-	const double	del_x = (*beg_x - *end_x);
-	const double	del_y = (*beg_y - *end_y);
+    const double	delta_x = (*start_x - *end_x);
+    const double	delta_y = (*start_y - *end_y);
 
-	*beg_x = (del_x * cos(-ROTATE_SPEED) - del_y * sin(-ROTATE_SPEED)) + *end_x;
-	*beg_y = (del_x * sin(-ROTATE_SPEED) + del_y * cos(-ROTATE_SPEED)) + *end_y;
+    *start_x = (delta_x * cos(-ROTATE_SPEED) - delta_y * sin(-ROTATE_SPEED)) + *end_x;
+    *start_y = (delta_x * sin(-ROTATE_SPEED) + delta_y * cos(-ROTATE_SPEED)) + *end_y;
 }
 
 void	turn_left(t_data *data)
 {
-	rotate_vec_back(&data->dir.x, &data->dir.y, &data->player.x,
-		&data->player.y);
-	rotate_vec_back(&data->plane.x, &data->plane.y, &data->player.x,
-		&data->player.y);
-	rotate_vec_back(&data->plane2.x, &data->plane2.y, &data->player.x,
-		&data->player.y);
+    rotate_vector_back(&data->dir.x, &data->dir.y, &data->player.x,
+        &data->player.y);
+    rotate_vector_back(&data->plane.x, &data->plane.y, &data->player.x,
+        &data->player.y);
+    rotate_vector_back(&data->plane2.x, &data->plane2.y, &data->player.x,
+        &data->player.y);
 }
 
 void	turn_right(t_data *data)
 {
-	rotate_vec(&data->dir.x, &data->dir.y, &data->player.x, &data->player.y);
-	rotate_vec(&data->plane.x, &data->plane.y, &data->player.x,
-		&data->player.y);
-	rotate_vec(&data->plane2.x, &data->plane2.y, &data->player.x,
-		&data->player.y);
+    rotate_vector(&data->dir.x, &data->dir.y, &data->player.x, &data->player.y);
+    rotate_vector(&data->plane.x, &data->plane.y, &data->player.x,
+        &data->player.y);
+    rotate_vector(&data->plane2.x, &data->plane2.y, &data->player.x,
+        &data->player.y);
 }
