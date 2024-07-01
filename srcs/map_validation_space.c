529a4829e3d31730abd561d30c1f0fb710d87804
Md Ohiduzzaman Sumon <msumon@c4r7p1.42vienna.com>
2024-07-01 17:09:18 +0200

    space fixed

--- srcs/map_validation_space.c
+++ srcs/map_validation_space.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 15:37:05 by msumon            #+#    #+#             */
-/*   Updated: 2024/07/01 16:14:22 by msumon           ###   ########.fr       */
+/*   Updated: 2024/07/01 17:07:46 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,41 @@
 
 int	check_middle(t_data *data, size_t x, int y)
 {
+	size_t	above_len;
+	size_t	below_len;
+
+	above_len = ft_strlen(data->map[y - 1]) - 1;
+	below_len = ft_strlen(data->map[y + 1]) - 1;
 	if (data->map[y][x - 1] != '1' && data->map[y][x - 1] != ' ')
 		return (1);
 	if (data->map[y][x + 1] != '1' && data->map[y][x + 1] != ' ')
 		return (1);
-	if (ft_strlen(data->map[y]) <= ft_strlen(data->map[y - 1])
-		&& ft_strlen(data->map[y]) <= ft_strlen(data->map[y + 1]))
+	if ((ft_strlen(data->map[y]) <= above_len)
+		&& (ft_strlen(data->map[y]) <= below_len))
 	{
 		if (data->map[y - 1][x] != '1' && data->map[y - 1][x] != ' ')
 			return (1);
 		if (data->map[y + 1][x] != '1' && data->map[y + 1][x] != ' ')
 			return (1);
 	}
-	if (x > ft_strlen(data->map[y - 1]) && x <= ft_strlen(data->map[y + 1]))
+	if ((x > above_len) && x <= below_len)
 	{
 		if (data->map[y + 1][x] != '1' && data->map[y + 1][x] != ' ')
 			return (1);
 	}
-	if (x < ft_strlen(data->map[y - 1]) && x > ft_strlen(data->map[y + 1]))
-	{
-		if (data->map[y - 1][x] != '1' && data->map[y - 1][x] != ' ')
-			return (1);
-	}
+	if ((x >= above_len) && x >= below_len)
+		return (0);
 	return (0);
 }
 
 int	space_checker(t_data *data, size_t x, int y)
 {
 	size_t	width;
+	size_t	above_len;
+	size_t	below_len;
 
+	above_len = ft_strlen(data->map[y - 1]) - 1;
+	below_len = ft_strlen(data->map[y + 1]) - 1;
 	width = ft_strlen(data->map[y]);
 	if (y == 0 && data->map[y + 1][x] != '1' && data->map[y + 1][x] != ' ')
 		return (1);
@@ -51,6 +57,11 @@ int	space_checker(t_data *data, size_t x, int y)
 		return (1);
 	else if (y > 0 && y < data->map_height - 1 && x < width - 1 && x > 0)
 	{
+		if ((x < above_len) && x > below_len)
+		{
+			if (data->map[y - 1][x] != '1' && data->map[y - 1][x] != ' ')
+				return (1);
+		}
 		if (check_middle(data, x, y))
 			return (1);
 	}
